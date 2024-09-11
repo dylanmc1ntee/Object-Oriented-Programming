47b7f985a0dd75391c30eb539773e650fe663fd0
dylanmc1ntee <dylangmcintee@gmail.com>
2024-09-11 13:12:51 -0400

    first line

--- Receipt-Gen/src/assignment1/McIntee_Dylan.java
+++ Receipt-Gen/src/assignment1/McIntee_Dylan.java
@@ -1,2 +1,10 @@
 package assignment1;
 
+public class McIntee_Dylan{
+
+    public static void main(String[] args) {
+
+        System.out.println("******************************");
+        
+    }
+}
