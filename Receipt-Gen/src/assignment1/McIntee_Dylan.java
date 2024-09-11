67240b62cae11f768ee35e5ecf138cde4c4f97a8
dylanmc1ntee <dylangmcintee@gmail.com>
2024-09-11 13:16:13 -0400

    welcome message

--- Receipt-Gen/src/assignment1/McIntee_Dylan.java
+++ Receipt-Gen/src/assignment1/McIntee_Dylan.java
@@ -5,6 +5,8 @@ public class McIntee_Dylan{
     public static void main(String[] args) {
 
         System.out.println("******************************");
+        System.out.println("****** S store ***************");
+        System.out.println("******************************");
         
     }
 }
