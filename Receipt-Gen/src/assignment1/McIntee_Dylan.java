15e0c20e5fec22060877803db8b854f39eb8fc1a
dylanmc1ntee <dylangmcintee@gmail.com>
2024-09-12 21:58:18 -0400

    without rounding

--- Receipt-Gen/src/assignment1/McIntee_Dylan.java
+++ Receipt-Gen/src/assignment1/McIntee_Dylan.java
@@ -12,121 +12,151 @@ public class McIntee_Dylan{
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
         String itemName;
-        
-        do {
+        double totalPrice = 0.0;
+        int numItems = 0;
+        final double TAX = 1.30;
 
-            System.out.print("receipt number      ");
-            int rNum = random.nextInt(1000) + 1000;
-            System.out.println(rNum);
+        System.out.print("receipt number      ");
+        int rNum = random.nextInt(1000) + 1000;
+        System.out.println(rNum);
 
-            int rMonth = random.nextInt(12);
-            int rDay = random.nextInt(28) + 1;
-            int rYear = random.nextInt(200) + 1900;
+        int rMonth = random.nextInt(12);
+        int rDay = random.nextInt(28) + 1;
+        int rYear = random.nextInt(200) + 1900;
 
-            if (rMonth == 0) {
+        if (rMonth == 0) {
 
-                System.out.print("January ");
+            System.out.print("January ");
+                
+        } else {
+
+            if (rMonth == 1) {
+
+                System.out.print("February ");
                 
             } else {
 
-                if (rMonth == 1) {
+                if (rMonth == 2) {
 
-                    System.out.print("February ");
-                
+                    System.out.print("March ");
+                    
                 } else {
-
-                    if (rMonth == 2) {
-
-                        System.out.print("March ");
-                    
-                    } else {
     
-                        if (rMonth == 3) {
+                    if (rMonth == 3) {
 
-                            System.out.print("April ");
+                        System.out.print("April ");
                         
-                        } else {
+                    } else {
         
-                            if (rMonth == 4) {
+                        if (rMonth == 4) {
 
-                                System.out.print("May ");
+                            System.out.print("May ");
                             
-                            } else {
+                        } else {
             
-                                if (rMonth == 5) {
+                            if (rMonth == 5) {
 
-                                    System.out.print("June ");
+                                System.out.print("June ");
                                 
-                                } else {
+                            } else {
                 
-                                    if (rMonth == 6) {
+                                if (rMonth == 6) {
 
-                                        System.out.print("July ");
+                                    System.out.print("July ");
                                     
+                                } else {
+                    
+                                    if (rMonth == 7) {
+
+                                        System.out.print("August ");
+                                        
                                     } else {
-                    
-                                        if (rMonth == 7) {
+                        
+                                        if (rMonth == 8) {
 
-                                            System.out.print("August ");
-                                        
+                                            System.out.print("September ");
+                                            
                                         } else {
-                        
-                                            if (rMonth == 8) {
+                            
+                                            if (rMonth == 9) {
 
-                                                System.out.print("September ");
-                                            
+                                                System.out.print("October ");
+                                                
                                             } else {
-                            
-                                                if (rMonth == 9) {
+                                
+                                                if (rMonth == 10) {
 
-                                                    System.out.print("October ");
-                                                
+                                                    System.out.print("November ");
+                                                    
                                                 } else {
-                                
-                                                    if (rMonth == 10) {
+                                    
+                                                    if (rMonth == 11) {
 
-                                                        System.out.print("November ");
-                                                    
+                                                        System.out.print("December ");
+                                                        
                                                     } else {
-                                    
-                                                        if (rMonth == 11) {
-
-                                                            System.out.print("December ");
-                                                        
-                                                        } else {
                                         
-                                                            System.out.print("ERROR ");
+                                                        System.out.print("ERROR ");
                                                             
-                                                        }
-                                                        
                                                     }
-                                                    
+                                                        
                                                 }
-                                                
+                                                    
                                             }
-                                            
+                                                
                                         }
-                                        
+                                            
                                     }
-                                    
+                                        
                                 }
-                                
+                                    
                             }
-                            
+                                
                         }
-                        
+                            
                     }
-                    
+                        
                 }
+                    
+            }
+
+        }
+
+        System.out.print(rDay + " ");
+        System.out.println(rYear);
+
+        System.out.print("Write item name ");
+        itemName = scan.nextLine();
+        
+        do {
+
+            System.out.print("Write price ");
+            double curItem = scan.nextDouble();
+            scan.nextLine();
+
+            System.out.print("Item number " + numItems + " " + itemName + "  ");
+            
+            if(itemName.equals("Food") || itemName.equals("food")) {
+
+                System.out.println(curItem);
+                totalPrice += curItem;
+                numItems++;
+
+            }
+            else {
+
+                System.out.println((curItem * TAX));
+                totalPrice += (curItem * TAX);
+                numItems++;
 
             }
 
-            System.out.print(rDay + " ");
-            System.out.println(rYear);
-
             System.out.print("Write item name ");
             itemName = scan.nextLine();
+
         } while (!(itemName.equals("DONE")));
+
+        System.out.println(numItems + " items      total " + totalPrice);
 
         System.out.println("******************************");
         System.out.println("******************************");
