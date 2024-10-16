1d934371c8b221e66c00fb3056908f4745a8ce99
dylanmc1ntee <dylangmcintee@gmail.com>
2024-10-16 12:25:14 -0400

    add graph

--- Dice-Simulation/src/assignment2/Mcintee_Dylan.java
+++ Dice-Simulation/src/assignment2/Mcintee_Dylan.java
@@ -14,6 +14,18 @@ public class Mcintee_Dylan {
         for(int i = 0; i < values.length; i++){
             System.out.println("Number of " + (i+2) + "s are " + values[i]);
         }
+
+        System.out.println("Graph");
+        System.out.println("175|                               ");
+        System.out.println("150|               *               ");
+        System.out.println("125|            *  *  *            ");
+        System.out.println("100|         *  *  *  *  *         ");
+        System.out.println(" 75|      *  *  *  *  *  *  *      ");
+        System.out.println(" 50|   *  *  *  *  *  *  *  *  *   ");
+        System.out.println(" 25|*  *  *  *  *  *  *  *  *  *  *");
+        System.out.println("  0|*  *  *  *  *  *  *  *  *  *  *");
+        System.out.println("   |--------------------------------");
+        System.out.println("    2  3  4  5  6  7  8  9 10 11 12");
     }
 
 }
